0b76e240f09ad177ce6ff7c41cf9d3d3e8563bb8
hayoun <123635639+Ha-youn@users.noreply.github.com>
2025-05-07 21:29:57 +0900

    Delete ocean_bench_test.py

--- ocean_bench_test.py
+++
@@ -1,9 +0,0 @@
-# ocean_bench_test.py
-import xarray as xr
-import numpy as np
-import matplotlib.pyplot as plt
-
-# 1. 파일 열기
-ds = xr.open_dataset("C:/Users/danah/Desktop/dc_obs/dc_obs/2020a_SSH_mapping_NATL60_envisat.nc")
-
-print(ds.variables)
